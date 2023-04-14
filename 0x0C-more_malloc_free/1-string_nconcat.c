3a837212d8fa721f587dffa2fa69fa4460873757
Hicham1ch <Hicham111.hm0@gmail.com>
2023-04-13 19:54:15 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,11 +3,11 @@
 
 /**
  * *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
+ * @s1: string to append to.
+ * @s2: string to concatenate from.
+ * @n: number of bytes from s2 to concatenate to s1.
  *
- * Return: pointer to the resulting string
+ * Return: pointer to the resulting string.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -24,7 +24,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
-
 	if (!s)
 		return (NULL);
 
@@ -44,3 +43,4 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	return (s);
 }
+
